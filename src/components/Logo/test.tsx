e065f994bf3c71aa3dfda59984bb7b85931a26e7
Lucas Pazzim <lpazzim@gmail.com>
2021-08-24 19:28:23 -0300

    updating config + tests Logo component

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
-describe('', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+describe('<Logo />', () => {
+  it('should render a white label by defatul', () => {
+    renderWithTheme(<Logo />)
 
-    // expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
