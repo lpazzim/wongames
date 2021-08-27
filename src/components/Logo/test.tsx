69f32e62783a1f1a714b5f4a1d5810ca942ca5fa
Lucas Pazzim <lpazzim@gmail.com>
2021-08-26 21:56:06 -0300

    Logo finished

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -18,4 +18,29 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
+  })
 })
