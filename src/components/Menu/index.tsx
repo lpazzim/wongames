efc0705ffa5ac9d3f6c9a009d9bf28152e0430b6
Lucas Pazzim <lpazzim@gmail.com>
2021-09-15 23:38:46 -0300

    menu component tests

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,7 +8,9 @@ import Logo from 'components/Logo'
 import * as S from './styles'
 import Button from 'components/Button'
 
-const Menu = () => {
+export type MenuProps = { userName?: string }
+
+const Menu = ({ userName }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <S.Wrapper>
@@ -31,16 +33,26 @@ const Menu = () => {
         <S.MenuNav>
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
+
+          {!!userName && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">WishList</S.MenuLink>
+            </>
+          )}
         </S.MenuNav>
-        <S.RegisterBox>
-          <Button fullWidth size="large">
-            Log in now
-          </Button>
-          <span>or</span>
-          <S.CreateAccount href="" title="Sign Up">
-            Sign Up
-          </S.CreateAccount>
-        </S.RegisterBox>
+
+        {!userName && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
       </S.MenuFull>
     </S.Wrapper>
   )
