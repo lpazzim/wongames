159fd3f98b99c01c1ddf4ebb099bd4eaa50f2751
Lucas Pazzim <lpazzim@gmail.com>
2021-09-17 18:45:16 -0300

    Heading component adjust

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    size={size}
+    lineColor={lineColor}
+  >
     {children}
   </S.Wrapper>
 )
