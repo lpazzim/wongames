e2329e3a76dd6c06a8f47bfd0334559e5b9b52a4
Lucas Pazzim <lpazzim@gmail.com>
2021-09-16 23:44:51 -0300

    Menu component finished

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,8 +5,9 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from 'components/Logo'
-import * as S from './styles'
 import Button from 'components/Button'
+import MediaMatch from 'components/MediaMatch'
+import * as S from './styles'
 
 export type MenuProps = { userName?: string }
 
@@ -14,12 +15,22 @@ const Menu = ({ userName }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <S.Wrapper>
-      <S.IconWrapper>
-        <MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper>
+          <MenuIcon aria-label="Open Menu" onClick={() => setIsOpen(true)} />
+        </S.IconWrapper>
+      </MediaMatch>
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="Search" />
@@ -27,6 +38,11 @@ const Menu = ({ userName }: MenuProps) => {
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+        {!userName && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign In</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -30,6 +30,7 @@ export const MenuGroup = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-grow: 1;
+    align-items: center;
     justify-content: flex-end;
     > div {
       margin-left: ${theme.spacings.xsmall};
@@ -37,11 +38,18 @@ export const MenuGroup = styled.div`
   `}
 `
 
-export const MenuNav = styled.div``
+export const MenuNav = styled.div`
+  ${({ theme }) => css`
+    ${media.greaterThan('medium')`
+  margin-left: ${theme.spacings.small};
+  `}
+  `}
+`
 
 export const MenuLink = styled.a`
   ${({ theme }) => css`
     position: relative;
+    color: ${theme.colors.white};
     font-size: ${theme.font.sizes.medium};
     margin: 0.3rem ${theme.spacings.small} 0;
     text-decoration: none;
