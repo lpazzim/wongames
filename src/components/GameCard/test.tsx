9688e30b06c8c2a1c12860ec1df4488ef1cb5375
Lucas Pazzim <lpazzim@gmail.com>
2021-10-04 20:01:14 -0300

    First version GameCard

---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import GameCard from '.'
+
+describe('', () => {
+  it('should render correctly', () => {
+    // const { container } = render(<GameCard />)
+    // expect(
+    //   screen.getByRole('heading', { name: /GameCard/i })
+    // ).toBeInTheDocument()
+    // expect(container.firstChild).toMatchSnapshot()
+  })
+})
