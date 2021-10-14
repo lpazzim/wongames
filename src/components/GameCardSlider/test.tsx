261c978db69db2a17fa9a8e74e39341d375635e2
Lucas Pazzim <lpazzim@gmail.com>
2021-10-14 07:56:07 -0300

    Create GameCardSlider + tests

---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,69 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameCardSlider from '.'
+
+const items = [
+  {
+    title: 'Population Zero 1',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero 5',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
+describe('', () => {
+  it('should render game slider', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(container.querySelector('.slick-slider')).toBeInTheDocument()
+  })
+
+  it('should render with 1 active item', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(5)
+    // expect(container.querySelectorAll('li.slick-active')).toHaveLength(4)
+
+    expect(
+      screen.getByRole('heading', { name: /population zero 1/i, hidden: false })
+    ).toBeInTheDocument()
+  })
+
+  it('should render with the arrows', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(container.querySelector('.slick-prev')).toBeInTheDocument()
+    expect(container.querySelector('.slick-next')).toBeInTheDocument()
+  })
+})
