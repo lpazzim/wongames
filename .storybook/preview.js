14edec95c775bb6f1764088c3412f79a33363658
Lucas Pazzim <lpazzim@gmail.com>
2021-10-22 20:06:26 -0300

    Storybook background standard colors config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,14 +4,20 @@ import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg,
+      },
+    ],
   },
-}
+};
 
 export const decorators = [
   (Story) => (
