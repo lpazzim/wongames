d3b52a7e4ee7872c0c6ee548741cc4954d78ab21
Lucas Pazzim <lpazzim@gmail.com>
2021-10-31 09:52:58 -0300

    Home tests optimization

--- src/components/Highlight/mock.ts
+++ src/components/Highlight/mock.ts
@@ -1,5 +1,5 @@
 export default {
-  title: `Red Dead it's back`,
+  title: `Red Dead is back!`,
   subtitle: `Come see John's new adventures`,
   backgroundImage: '/img/red-dead-img.jpg',
   buttonLabel: 'Buy Now',
