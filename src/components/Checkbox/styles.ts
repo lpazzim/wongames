8188b3c0561e4208362e09007c694e732559acf6
Lucas Pazzim <lpazzim@gmail.com>
2021-11-02 09:48:07 -0300

    Checkbox style

--- src/components/Checkbox/styles.ts
+++ src/components/Checkbox/styles.ts
@@ -1,3 +1,11 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import theme from 'styles/theme'
+import { CheckboxProps } from '.'
 
 export const Wrapper = styled.main``
+
+export const Label = styled.label<Pick<CheckboxProps, 'labelColor'>>`
+  ${({ theme, labelColor }) => css`
+    color: ${theme.colors[labelColor!]};
+  `}
+`
