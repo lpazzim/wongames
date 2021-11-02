b886135721e2af78bc68b4e05cdfb2edcef20b34
Lucas Pazzim <lpazzim@gmail.com>
2021-11-02 08:30:47 -0300

    Generate Checkbox

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+const Checkbox = () => (
+  <S.Wrapper>
+    <input type="checkbox" id="action" />
+    <label htmlFor="action">Action</label>
+  </S.Wrapper>
+)
+
+export default Checkbox
