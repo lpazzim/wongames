0fde945c690da79dffcb213ae09a211cc3fe0ade
Lucas Pazzim <lpazzim@gmail.com>
2022-01-27 18:00:22 -0300

    Templates and pages SignIn and SignUp

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -3,8 +3,12 @@ import { render, screen } from '@testing-library/react'
 import Auth from '.'
 
 describe('', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Auth />)
+  it('should render all components and children', () => {
+    const { container } = render(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
 
     expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
 
